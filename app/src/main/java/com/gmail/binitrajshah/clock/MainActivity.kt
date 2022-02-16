f60dc804180a6584d56ccad45c103d6a4bf4eb3f
Binit Kumar Shah <binitrajshah931@gmail.com>
2022-02-16 10:20:26 +0530

    Designed Main Activity Layout

--- app/src/main/java/com/gmail/binitrajshah/clock/MainActivity.kt
+++ app/src/main/java/com/gmail/binitrajshah/clock/MainActivity.kt
@@ -2,10 +2,23 @@ package com.gmail.binitrajshah.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.gmail.binitrajshah.clock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        
+
     }
 }
